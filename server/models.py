b7ad76ec11570dfd5fd35d0a2901b012eb137cfb
Watheq Attrah <novaatrah@gmail.com>
2023-09-17 15:39:51 -0700

    Complete assign

--- server/models.py
+++ server/models.py
@@ -6,22 +6,7 @@ db = SQLAlchemy()
 
 class Author(db.Model):
     __tablename__ = "authors"
-
     # Add validations and constraints
-    @validates("name")
-    def validate_name(self, key, name):
-        if not name:
-            raise ValueError
-        existing_author = Author.query.filter(Author.name == name).first()
-        if existing_author and existing_author.id != self.id:
-            raise ValueError
-        return name
-
-    @validates("phone_number")
-    def validate_phone_number(self, key, number):
-        if len(number) != 10:
-            raise ValueError
-        return number
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, unique=True, nullable=False)
@@ -29,44 +14,28 @@ class Author(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
+    @validates("name")
+    def validate_name(self, key, name):
+        names = db.session.query(Author.name).all()
+        if not name:
+            raise ValueError("Name field is required.")
+        elif name in names:
+            raise ValueError("Name must be unique.")
+        return name
+
+    @validates("phone_number")
+    def validate_phone_number(self, key, phone_number):
+        if len(phone_number) != 10:
+            raise ValueError("Phone number must be 10 digits.")
+        return phone_number
+
     def __repr__(self):
         return f"Author(id={self.id}, name={self.name})"
 
 
 class Post(db.Model):
     __tablename__ = "posts"
-
     # Add validations and constraints
-    @validates("title")
-    def validate_title(self, key, title):
-        if not title:
-            raise ValueError
-        if (
-            "Won't Believe" not in title
-            or "Secret" not in title
-            or "Top" not in title
-            or "Guess" not in title
-        ):
-            raise ValueError
-        return title
-
-    @validates("content")
-    def validate_content(self, key, content):
-        if len(content) < 250:
-            raise ValueError
-        return content
-
-    @validates("summary")
-    def validate_summary(self, key, summary):
-        if len(summary) >= 250:
-            raise ValueError
-        return summary
-
-    @validates("category")
-    def validate_category(self, key, category):
-        if category != "Fiction" and category != "Non-Fiction":
-            raise ValueError
-        return category
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
@@ -75,6 +44,33 @@ class Post(db.Model):
     summary = db.Column(db.String)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    @validates("title")
+    def validate_title(self, key, title):
+        clickbait = ["Won't Believe", "Secret", "Top", "Guess"]
+        if not any(substring in title for substring in clickbait):
+            raise ValueError("No clcikbait found")
+        return title
+
+    @validates("content", "summary")
+    def validate_length(self, key, string):
+        if key == "content":
+            if len(string) <= 250:
+                raise ValueError(
+                    "Post content must be greater than or equal 250 characters long."
+                )
+        elif key == "summary":
+            if len(string) >= 250:
+                raise ValueError(
+                    "Post summary must be less than or equal to 250 characters long."
+                )
+        return string
+
+    @validates("category")
+    def validate_category(self, key, category):
+        if category != "Fiction" and category != "Non-Fiction":
+            raise ValueError("Category must be Fiction or Non-Fiction")
+        return category
 
     def __repr__(self):
         return f"Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})"
